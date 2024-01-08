762a03dde89b11628442d6c348c43a8c20123c59
ShadowDragon2040 <112861430+ShadowDragon2040@users.noreply.github.com>
2024-01-08 12:27:47 +0100

    token generálás

---
+++ Webárúház_Nagy_Project/Controllers/Bejelenkezes.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webárúház_Nagy_Project.Models;
+using Webárúház_Nagy_Project.DTOs;
+using Org.BouncyCastle.Crypto.Generators;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Webárúház_Nagy_Project.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Bejelenkezes : Controller
+    {
+        public static Felhasznalok user = new Felhasznalok();
+        private readonly IConfiguration  _configuration;
+
+        public Bejelenkezes(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public ActionResult<Felhasznalok> Register(UserDto request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.LoginNev = request.Username;
+            user.Hash = request.Password;
+            
+            return Ok(user);
+        }
+
+        [HttpPost("Login")]
+        public ActionResult<Felhasznalok> Login(UserDto request)
+        {
+            if (user.LoginNev != request.Username)
+            {
+                return BadRequest("Ilyen nevű felhasználó nincsen.");
+
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Hash))
+            {
+                return BadRequest("Rossz jelszó.");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+        private string CreateToken(Felhasznalok user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.LoginNev)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSetting:Token").Value!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
--- Webárúház_Nagy_Project/DTOs/FelhasznalokDTO.cs
+++ Webárúház_Nagy_Project/DTOs/FelhasznalokDTO.cs
@@ -5,4 +5,10 @@ namespace Webárúház_Nagy_Project.DTOs
     public record FelhasznalokDto(Guid Id, string LoginNev, string Hash, string Salt, string Nev, int Jog, bool Aktivalva, string Email, string ProfilKep, Orszagok OrszágKod, Varosok VarosNev, string UtcaNev, string IranyitoSzam, int Hazszam);
     public record CreatedFelhasznalokDto(string LoginNev, string Hash, string Salt, string Nev, int Jog, bool Aktivalva, string Email, string ProfilKep, Orszagok OrszágKod, Varosok VarosNev, string UtcaNev, string IranyitoSzam, int Hazszam);
     public record UpdateFelhasznalokDto(string LoginNev, string Hash, string Salt, string Nev, int Jog, bool Aktivalva, string Email, string ProfilKep, Orszagok OrszágKod, Varosok VarosNev, string UtcaNev, string IranyitoSzam, int Hazszam);
+
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
