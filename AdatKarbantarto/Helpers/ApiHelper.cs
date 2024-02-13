4f81ecf6406a8c4f18fa1fa9efb2ffab7b83e0a7
pejkob <112940859+pejkob@users.noreply.github.com>
2024-02-13 18:53:10 +0100

    Api helper létrehozva

---
+++ AdatKarbantarto/Helpers/ApiHelper.cs
@@ -0,0 +1,44 @@
+﻿using AdatKarbantarto.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatKarbantarto.Helpers
+{
+    public class ApiHelper 
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiHelper()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://your-api-base-url.com/api/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> GetAsync<T>(string endpoint)
+        {
+            using (HttpResponseMessage response = await _httpClient.GetAsync(endpoint))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<AuthenticatedUser>(content);
+                }
+                else
+                {
+                    throw new Exception($"Failed to fetch data from API. Status code: {response.StatusCode}");
+                }
+            }
+        }
+
+    }
+}
---
+++ AdatKarbantarto/Model/AuthenticatedUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatKarbantarto.Model
+{
+    public class AuthenticatedUser
+    {
+        public string AccesToken { get; set; }
+        public string UserName { get; set;}
+    }
+}
