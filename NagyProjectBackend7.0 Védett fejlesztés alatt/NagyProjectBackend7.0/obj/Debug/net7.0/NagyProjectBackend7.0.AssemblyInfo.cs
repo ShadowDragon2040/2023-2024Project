08cc8bbe3e0b6dc1d40e273df0f4c441a4fc01ad
ShadowDragon2040 <112861430+ShadowDragon2040@users.noreply.github.com>
2024-02-19 09:44:51 +0100

    frontend termek single
    javitasok

--- NagyProjectBackend7.0 Védett fejlesztés alatt/NagyProjectBackend7.0/obj/Debug/net7.0/NagyProjectBackend7.0.AssemblyInfo.cs
+++ NagyProjectBackend7.0 Védett fejlesztés alatt/NagyProjectBackend7.0/obj/Debug/net7.0/NagyProjectBackend7.0.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NagyProjectBackend7.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a75e4ec3f28df509e77cd92b14cc14a839a569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332aeea340418c2140eb565fb379f2d6f5bbe92d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NagyProjectBackend7.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NagyProjectBackend7.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/pages/SingleProductDisplay.js
+++ frontend/src/pages/SingleProductDisplay.js
@@ -8,19 +8,15 @@ import 'react-comments-section/dist/index.css';
 import Navbar from '../components/Navbar';
 import {Rating} from 'react-simple-star-rating';
 import Footer from '../components/Footer';
-import { TbSend } from "react-icons/tb";
 
 function SingleProductDisplay() {
     const { ProductId } = useParams();
-    const [itemData, setItemData] = useState({});
-    const [commentsData, setCommentData] = useState([]);
     const [rating, setRating] = useState(0);
     const [commentString, setCommentString] = useState("");
 
     const handleRating = (rate) => {
         setRating(rate);
     }
-
 
     const handleCommentSubmit = async () => {
         try {
@@ -33,29 +29,28 @@ function SingleProductDisplay() {
         }
     };
 
+    const [singleProductData, setSingleProductData] = useState([]);
+    const [transformedComments, setTransformedComments] = useState(null);
+    const url = "http://localhost:5219/Termekek/EgyTermek/";
+    
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5219/Termekek/${ProductId}`);
-                setItemData(response.data[0]);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        const fetchComments = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5219/Hozzaszolas/termek/${ProductId}`);
-                setCommentData(response.data); 
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchData();
-        fetchComments();
+        axios.get(url + ProductId)
+            .then(response => {
+                setSingleProductData(response.data);
+                const transformedComments = response.data.hozzaszolasok.map(productData => ({
+                    userId: productData.felhasznaloId.toString(),
+                    comId: productData.hozzaszolasId.toString(),
+                    fullName: productData.loginNev,
+                    avatarUrl: '', 
+                    text: productData.leiras,
+                    userProfile: '', 
+                    replies: []
+                }));
+                setTransformedComments(transformedComments);
+            })
+            .catch(error => console.error('Error fetching product data:', error));
     }, [ProductId]);
-
+    
 
     return (
         <>
@@ -65,44 +60,40 @@ function SingleProductDisplay() {
                     <NavLink to={"/ShopPage"} className='back-btn'>
                         <MdArrowBack />
                     </NavLink>
-                        <div className=' rounded mx-auto d-flex flex-row mt-2 FirstRow'>
-                            <InnerImageZoom
-                                className='ImageZoom w-100'
-                                src={"/" + itemData.keputvonal}
-                                zoomSrc={"/" + itemData.keputvonal}
-                                fullscreenOnMobile={true}
-                                moveType="drag"
-                                zoomScale={1.3}
-                                zoomPreload={true}
-                            />
-                            <div className='card-body rounded w-100 p-4'>
-                                <h4>{itemData.termekNev}</h4>
-                                <p>{itemData.leiras}</p>
-                                {itemData.menyiseg && <p>Available: {itemData.menyiseg} pieces</p>}
-                                {itemData.ar && <h3>{itemData.ar} -Ft</h3>}
-                            </div>
+                    <div className=' rounded mx-auto d-flex flex-row mt-2 FirstRow'>
+                        <InnerImageZoom
+                            className='ImageZoom w-100'
+                            src={"/" + singleProductData.keputvonal}
+                            zoomSrc={"/" + singleProductData.keputvonal}
+                            fullscreenOnMobile={true}
+                            moveType="drag"
+                            zoomScale={1.3}
+                            zoomPreload={true}
+                        />
+                        <div className='card-body rounded w-100 p-4'>
+                            <h4>{singleProductData.termekNev}</h4>
+                            <p>{singleProductData.termekLeiras}</p>
+                            {singleProductData.menyiseg && <p>Available: {singleProductData.menyiseg} pieces</p>}
+                            {singleProductData.ar && <h3>{singleProductData.ar} -Ft</h3>}
                         </div>
-                    
+                    </div>
+
                     <div className="row align-items-start">
                         <div className='rounded mx-auto d-flex flex-row'>
                             <div className='wrapper card-body rounded mt-3 w-100 p-2'>
-                                    <Rating
-                                        onClick={handleRating}
-                                    />
-                                    <CommentSection
-                                      
-                                        currentUser={{
-                                            currentUserId: 'YourUserId',
-                                            currentUserFullName: 'YourUserName',
-                                            currentUserImg: 'YourUserImageURL'
-                                        }}
-                                    />
-                                
-                                   
-                              
+                                <Rating
+                                    onClick={handleRating}
+                                />
+                                <CommentSection
+                                    currentUser={{
+                                        currentUserId: 'YourUserId',
+                                        currentUserFullName: 'YourUserName',
+                                        currentUserImg: 'YourUserImageURL'
+                                    }}
+
+                                    commentData={transformedComments}
                                     
-                                 
-                                
+                                />
                             </div>
                         </div>
                     </div>
