e872b6cefc8ed3210cf2b79c450274783d0914c0
ShadowDragon2040 <112861430+ShadowDragon2040@users.noreply.github.com>
2024-03-05 08:21:00 +0100

    felesleg törlés

--- auth_3.sql
+++ auth_3.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Feb 06. 13:25
--- Kiszolgáló verziója: 10.4.28-MariaDB
--- PHP verzió: 8.2.4
+-- Létrehozás ideje: 2024. Már 04. 11:48
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,13 +49,6 @@ CREATE TABLE `aspnetroles` (
   `ConcurrencyStamp` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- A tábla adatainak kiíratása `aspnetroles`
---
-
-INSERT INTO `aspnetroles` (`Id`, `Name`, `NormalizedName`, `ConcurrencyStamp`) VALUES
-('93d4e9c4-1121-4a86-8d33-c591bb1c646c', 'ADMIN', 'ADMIN', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -93,13 +86,6 @@ CREATE TABLE `aspnetuserroles` (
   `RoleId` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- A tábla adatainak kiíratása `aspnetuserroles`
---
-
-INSERT INTO `aspnetuserroles` (`UserId`, `RoleId`) VALUES
-('8e2da1bf-1951-4ea5-b515-25342738fff4', '93d4e9c4-1121-4a86-8d33-c591bb1c646c');
-
 -- --------------------------------------------------------
 
 --
@@ -108,6 +94,7 @@ INSERT INTO `aspnetuserroles` (`UserId`, `RoleId`) VALUES
 
 CREATE TABLE `aspnetusers` (
   `Id` varchar(255) NOT NULL,
+  `EmailCode` int(11) NOT NULL,
   `UserName` varchar(256) DEFAULT NULL,
   `NormalizedUserName` varchar(256) DEFAULT NULL,
   `Email` varchar(256) DEFAULT NULL,
@@ -121,21 +108,15 @@ CREATE TABLE `aspnetusers` (
   `TwoFactorEnabled` tinyint(1) NOT NULL,
   `LockoutEnd` datetime(6) DEFAULT NULL,
   `LockoutEnabled` tinyint(1) NOT NULL,
-  `AccessFailedCount` int(11) NOT NULL,
-  `Age` int(11) NOT NULL DEFAULT 0,
-  `FullName` longtext NOT NULL
+  `AccessFailedCount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `aspnetusers`
 --
 
-INSERT INTO `aspnetusers` (`Id`, `UserName`, `NormalizedUserName`, `Email`, `NormalizedEmail`, `EmailConfirmed`, `PasswordHash`, `SecurityStamp`, `ConcurrencyStamp`, `PhoneNumber`, `PhoneNumberConfirmed`, `TwoFactorEnabled`, `LockoutEnd`, `LockoutEnabled`, `AccessFailedCount`, `Age`, `FullName`) VALUES
-('8e2da1bf-1951-4ea5-b515-25342738fff4', 'jani', 'JANI', 'jani01@gmail.com', 'JANI01@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEHI4dv5KGEivBQEQrmh78HGQpww77kvX3dy+xyulrR6fi7IzlzkqEDUvW4b9KE4aUA==', 'EI43ZSFFBU4JX3C7LA4FVDEF62Q433W4', '85923696-dfeb-4cc7-8ed7-040812390101', NULL, 0, 0, NULL, 1, 0, 10, 'jani'),
-('9c7732d8-8c94-4727-8396-62ed74e2995f', 'janika1', 'JANIKA1', 'vardaib@kkszki.hu', 'VARDAIB@KKSZKI.HU', 1, 'AQAAAAIAAYagAAAAEPPTRSJ4itzqhiVfzRiddOZL8U0e9KPpA0wmjt+w3umvb4kPJhec6UPquB4r16tj+A==', 'GFLGE4DOXZ44WNUYPOSLH37BJT5P4F4E', 'eb07b159-9cdc-470b-84e2-1a1d41280aa9', NULL, 0, 0, NULL, 1, 0, 0, 'string'),
-('d0b6b682-bdc4-4ed0-b3b2-a5c1e4305b30', 'jani2', 'JANI2', 'vardai.balazs22@gmail.com', 'VARDAI.BALAZS22@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEJBxuI11EnSo5JUNfUkRzyX0huQPQuth45p1dPcj15zNyxUpvEcoRKnGRjYzfWXKwQ==', '7YVX2BWLYFUKIY6SQXD3D5E45NQ4KLC7', '62b82345-d4b7-4ab6-967e-121f81113e45', NULL, 0, 0, NULL, 1, 0, 10, 'jani2'),
-('e4de76a3-5cde-4fc5-9202-70940f157a60', 'janika', 'JANIKA', 'string', 'STRING', 0, 'AQAAAAIAAYagAAAAEAX5Jzi4LyzvvpkR8YR/qmtcrR2DiZmnaNb7WJM1cBD5xDxBuhNbEu+8vgsfnsOoXA==', 'IPWRQV53JZG5ZOQ4HMQDLYK6CE7YIPTY', '4001c8fa-b0c7-4177-9339-36224d4382b5', NULL, 0, 0, NULL, 1, 0, 0, 'string'),
-('f85fd2f4-7641-41c7-9100-ea39fbc4554d', 'bela', 'BELA', 'string', 'STRING', 0, 'AQAAAAIAAYagAAAAEI9lQqviEuJEX/nzrTaEwpaXZk7mBjexD6vB3Dg8kA5tGM8Zs7Qoe6ZbfRRLJBkeoQ==', 'VXXCFYQHDGVDZMIIFTG4QGUVERSG6D2Q', '66814755-cdbf-4091-b719-9cde9cd74100', NULL, 0, 0, NULL, 1, 0, 20, 'bela');
+INSERT INTO `aspnetusers` (`Id`, `EmailCode`, `UserName`, `NormalizedUserName`, `Email`, `NormalizedEmail`, `EmailConfirmed`, `PasswordHash`, `SecurityStamp`, `ConcurrencyStamp`, `PhoneNumber`, `PhoneNumberConfirmed`, `TwoFactorEnabled`, `LockoutEnd`, `LockoutEnabled`, `AccessFailedCount`) VALUES
+('596f1b64-1338-4568-889e-d4bb0108f8a1', 6342, 'Balu', 'BALU', 'vardai.balazs22@gmail.com', 'VARDAI.BALAZS22@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEKJ1XE5LO04wTT+X5aG4zwdAh5T6TBfo9A7zSqRvZUHNVkY+s2knO346Y2bYAvbfZw==', '4LDJ56NOY6GFMWNI56KHD5FR5WFSXOEH', '7f6609ca-1c2b-458f-a14a-7fefdac1f969', NULL, 0, 0, NULL, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -166,9 +147,7 @@ CREATE TABLE `__efmigrationshistory` (
 --
 
 INSERT INTO `__efmigrationshistory` (`MigrationId`, `ProductVersion`) VALUES
-('20240123114739_IdentityDb', '8.0.1'),
-('20240123120845_MyProperties', '8.0.1'),
-('20240123121354_MyProperties', '8.0.1');
+('20240304075233_createIdentity', '8.0.1');
 
 --
 -- Indexek a kiírt táblákhoz
