ee9027f69db7fb3427afb0e9f13f398189326215
pejkob <112940859+pejkob@users.noreply.github.com>
2024-03-12 13:37:15 +0100

    Logout gomb létrehozva

--- AdatKarbantarto/View/HomeView.xaml.cs
+++ AdatKarbantarto/View/HomeView.xaml.cs
@@ -1,19 +1,5 @@
-﻿using AdatKarbantarto.Helpers;
-using AdatKarbantarto.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdatKarbantarto.View
 {
@@ -26,6 +12,15 @@ namespace AdatKarbantarto.View
         {
             InitializeComponent();
         }
-       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView loginWin = new LoginView();
+            loginWin.Show();
+
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.Close();
+
+        }
     }
 }
