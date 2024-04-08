982cf806268d1c6f651389580eed94bc565cfa93
pejkob <112940859+pejkob@users.noreply.github.com>
2024-04-08 11:53:12 +0200

    HomeView változtatások

--- authApi/authApi/Program.cs
+++ authApi/authApi/Program.cs
@@ -3,8 +3,12 @@ using authApi.Datas;
 using authApi.Models;
 using authApi.Services;
 using authApi.Services.IServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace authApi
 {
@@ -14,76 +18,36 @@ namespace authApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
+            builder.Services.AddCors(options =>
             {
-                var connectionString = builder.Configuration.GetConnectionString("Xampmashogyan");
-                options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+                options.AddPolicy(MyAllowSpecificOrigins,
+                                      policy =>
+                                      {
+                                          policy.WithOrigins("*")
+                                                                .AllowAnyHeader()
+                                                                .AllowAnyMethod()
+                                                                .AllowAnyOrigin();
+                                      });
             });
             builder.Services.AddDbContext<AuthContext>();
-
-
-            builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("AuthSettings:JwtOptions"));
-
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
-
-            //builder.Services.AddScoped<JwtOptions>();
-            builder.Services.AddScoped<IAuth, AuthService>();
-            builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-            // Add services to the container.
-
-            builder.Services.AddDbContext<AuthContext>();
-            builder.Services.AddAuthorization();
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            builder.Services.AddDbContext<AppDbContext>(option =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                var connectionString = builder.Configuration.GetConnectionString("Xampmashogyan");
+                option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
 
-            app.UseHttpsRedirection();
+            var settingsSection = builder.Configuration.GetSection("AuthSettings:JwtOptions");
 
-            app.UseCors("AllowOrigin");
+            builder.Services.Configure<JwtOptions>(settingsSection);
 
-
-
-            app.MapControllers();
-
-            app.Run();
-
-            //nagyprojectbackend program.cs
-            /*
-             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
-
-            builder.Services.AddDbContext<ProjectDatabaseContext>();
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-
-            //farkas módszer
-            builder.Services.AddSwaggerGen();
-            var settingsSection = builder.Configuration.GetSection("AuthSettings");
             var secret = settingsSection.GetValue<string>("Secret");
             var issuer = settingsSection.GetValue<string>("Issuer");
             var auidience = settingsSection.GetValue<string>("Audience");
 
             var key = Encoding.ASCII.GetBytes(secret);
 
-            //farkas módszer
             builder.Services.AddAuthentication(x =>
             {
                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -101,7 +65,21 @@ namespace authApi
                 };
             });
 
-           
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddScoped<IAuth, AuthService>();
+            builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
@@ -111,7 +89,8 @@ namespace authApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            app.UseCors("AllowOrigin");
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
 
@@ -122,7 +101,70 @@ namespace authApi
             app.MapControllers();
 
             app.Run();
-            */
+        
+
+        //nagyprojectbackend program.cs
+        /*
+         var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
+
+        builder.Services.AddDbContext<ProjectDatabaseContext>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+
+        //farkas módszer
+        builder.Services.AddSwaggerGen();
+        var settingsSection = builder.Configuration.GetSection("AuthSettings");
+        var secret = settingsSection.GetValue<string>("Secret");
+        var issuer = settingsSection.GetValue<string>("Issuer");
+        var auidience = settingsSection.GetValue<string>("Audience");
+
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        //farkas módszer
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidIssuer = issuer,
+                ValidAudience = auidience,
+                ValidateAudience = true
+            };
+        });
+
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+        app.UseCors("AllowOrigin");
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+        */
+    }
     }
 }
