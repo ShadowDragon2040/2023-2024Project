a86e80c186e46998a9b6dd1a0a2971cfd9dbf2a2
pejkob <112940859+pejkob@users.noreply.github.com>
2024-04-24 08:12:42 +0200

    CartPage megjelenés módisítás

--- frontend/src/pages/ShopPages/CartPage.js
+++ frontend/src/pages/ShopPages/CartPage.js
@@ -1,7 +1,7 @@
 //import React, { useState, useEffect } from "react";
 import { Button } from 'react-bootstrap';
 import {
-  NavBtnLink, ShopPageContainer,
+  NavBtnLink, NiceButton, ShopPageContainer,
 } from '../../components/TextElements';
 import { MdArrowBack } from "react-icons/md";
 import Footer from '../../components/FooterComponent';
@@ -15,15 +15,16 @@ const CartPage = ({ cart, setCart }) => {
   console.log(cart);
   const totalPayment = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
   return (
-
-    <ShopPageContainer>
+<>
 
       <Navbar/>
+    <ShopPageContainer>
 
-      <div className='container' style={{ height: '150vh' }}>
+
+      <div className='container' style={{ height: '150vh'}}>
         <div className='row'>
           <div className='col-xl-6'>
-            <div className="card" style={{ marginTop: "100px", height: '150vh', float: 'left' }}>
+            <div className="card" style={{ marginTop: "150px", height: '150vh',width:'85vh', float: 'left' }}>
               <div className="card-header">
                 <h5 className="card-title">Cart Items</h5>
               </div>
@@ -34,35 +35,44 @@ const CartPage = ({ cart, setCart }) => {
                 </div>
               ))}
 
-              <div className='mx-auto' style={{ alignSelf: 'flex-end', marginRight: '10px', marginTop: '50px', width: '75%' }}>
-                <h5>Total Payment: {totalPayment}</h5>
-                <div className="card-footer">
-                  <NavBtnLink to='/ShopPage'><MdArrowBack />Vissza a weboldalra</NavBtnLink>
-                  <Button variant="primary" onClick={() => console.log('Fizetés')}>Fizetés</Button>
-                </div>
-              </div>
+             
             </div>
           </div>
           <div className='col-xl-6'>
-            <div className='card' style={{marginTop: "100px", height: '50vh',width:'50vh', float: 'left' }}>
+            <div className='card' style={{marginTop: "150px", height: '50vh',width:'70vh', float: 'left' }}>
               <div className="card-header">
                 <h5 className="card-title">User Information</h5>
               </div>
               <div className='card-body'>
-                <img src="Profilkép" alt='profilkep' ></img>
+                <img src={`${process.env.REACT_APP_KEP_URL}ppp.jpg`} style={{borderRadius:"150px"}} className='mx-auto' width={"150px"} alt='profilkep' ></img>
                 <p>Username:</p>
                 <p>Address: </p>
                 <p>PhoneNumber: </p>
               </div>
             </div>
+            <div className='card' style={{marginTop: "50px", height: '25vh', width:'70vh' }}>
+            <div className='mx-auto' style={{ alignSelf: 'flex-end', marginTop: '50px', width: '80%' }}>
+                <h5>Total Payment: {totalPayment}</h5>
+                
+                <div className="row">
+                    <div className="col-6">
+                        <NavBtnLink to='/ShopPage'><MdArrowBack />Vissza a weboldalra</NavBtnLink>
+                    </div>
+                    <div className="col-6">
+                        <NavBtnLink variant="primary" onClick={() => console.log('Fizetés')}>Fizetés</NavBtnLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+
           </div>
         </div>
       </div>
 
-      <Footer className="fixed-bottom" isButtom={true}/>
 
     </ShopPageContainer>
-
+      <Footer className="fixed-bottom" isButtom={true}/>
+</>
 
   );
 
